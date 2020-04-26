93ee5528c4485bfb9e9f649738be47e55d120887
Brian Levin <bml201095@gmail.com>
2020-04-26 14:44:26 -0400

    made a variable to hold the questions which will hold the questions which will help run the inquire module.

--- index.js
+++ index.js
@@ -2,3 +2,26 @@ const inquirer = require('inquirer');
 const fs = require('fs')
 const path = require('path')
 const generateMarkdown = required('./generateMarkdown')
+
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'what is your github username?'
+
+
+
+
+    },
+    {
+
+    }
+
+
+
+
+
+
+
+
+]
